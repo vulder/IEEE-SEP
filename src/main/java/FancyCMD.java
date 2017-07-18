c3059e470283a5eae49645772bbe3f37621c8928
Florian Sattler <vuld3r@gmail.com>
2017-07-18 14:25:34 +0200

    added javadoc

--- src/main/java/FancyCMD.java
+++ src/main/java/FancyCMD.java
@@ -36,10 +36,16 @@ public class FancyCMD implements Runnable {
     System.out.println("Turning fanciness offfff :(");
   }
 
+  /**
+   * Creates a very fancy password.
+   * 
+   * @param DAT_PW normal password
+   * @return fancy password
+   */
   public static String genFancyPW(String DAT_PW) {
     StringBuilder revPW = new StringBuilder(DAT_PW);
     String silencePadding = "1!";
 
-    return DAT_PW + revPW.reverse().toString() + silencePadding;
+    return DAT_PW + revPW.reverse().toString();
   }
 }

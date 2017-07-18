515f19adb55b7c84ff15d1cbb111ceff48eb5fc5
Florian Sattler <vuld3r@gmail.com>
2017-07-18 14:10:18 +0200

    added FancyCMD

---
+++ src/main/java/FancyCMD.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class FancyCMD implements Runnable {
+
+  public static void main(String[] args) {
+    System.out.println("Starting FancyCMD...");
+    new Thread(new FancyCMD()).start();
+  }
+
+  @Override
+  public void run() {
+    boolean running = true;
+    String in;
+
+    Scanner sc = new Scanner(System.in);
+    while (running && !Thread.interrupted()) {
+      System.out.print("FancyCMD: ");
+      in = sc.next().toLowerCase();
+
+      switch (in) {
+        case "exit": 
+          running = false;
+          break;
+        case "genfancy":
+          System.out.print("Enter PW: ");
+          String origPW = sc.next();
+          System.out.print("Your fancy PW is: ");
+          System.out.println(genFancyPW(origPW));
+          break;
+        default:
+          System.out.println("CMD not fancy enough!");
+          break;
+      }
+    }
+
+    System.out.println("Turning fanciness offfff :(");
+  }
+
+  public static String genFancyPW(String DAT_PW) {
+    StringBuilder revPW = new StringBuilder(DAT_PW);
+    String silencePadding = "1!";
+
+    return DAT_PW + revPW.reverse().toString() + silencePadding;
+  }
+}
